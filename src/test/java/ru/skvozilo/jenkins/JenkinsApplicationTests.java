0c404aacbfcf42ec551df94ae1e700e33fdc742a
draft <draft85@list.ru>
2023-07-08 14:17:03 +0300

    .

---
+++ src/test/java/ru/skvozilo/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skvozilo.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
